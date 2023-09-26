e87b20d701025033b09819eb7e1cb67b3396370e
IJE2704 <mh1669101@gmail.com>
2023-09-26 08:29:38 +0600

    practice react project with firebase

---
+++ src/Components/__steps.js
@@ -0,0 +1,7 @@
+// 1. goto firebase console.google.firebase.com
+// 2. create new project
+// 3. create react project
+// 4. set react project with route
+// 5. install firebase with - npm install firebase
+// 6. create firebase folder in react project then create a file name : firebase.init.js
+// 7. copy the firebase sdk code and paste it in firebase.init.js
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFNJyRew3jsj_9kk7Qh3Wrhk_Tr84P__A",
+  authDomain: "simple-firebase-a42bc.firebaseapp.com",
+  projectId: "simple-firebase-a42bc",
+  storageBucket: "simple-firebase-a42bc.appspot.com",
+  messagingSenderId: "647839659081",
+  appId: "1:647839659081:web:e5e2c9962a63ad9a4dfb98"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
